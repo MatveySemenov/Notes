3dd6190f3d0700734379831a9943fc76a2d8b973
Matvey Semenov <semenov.matvej2017@gmail.com>
2024-01-19 15:52:08 +0700

    Сделал notes_book фрагментом, чтобы было проще использовать боковое меню

--- app/src/main/java/com/example/notes/notes_book.kt
+++ app/src/main/java/com/example/notes/notes_book.kt
@@ -2,115 +2,94 @@ package com.example.notes
 
 import android.app.Activity
 import android.content.Intent
-import androidx.fragment.app.Fragment
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notes.Adaptor.NotesAdaptor
 import com.example.notes.DataBase.EntityDataBase
 import com.example.notes.DataBase.NotesDataBase
 import com.example.notes.DataBase.NotesViewModel
-import com.example.notes.Fragments.sign_in
-import com.example.notes.R
 import com.example.notes.databinding.NotesBookBinding
-import com.example.notes.databinding.SignInBinding
-import com.example.notes.databinding.SignUpBinding
-import com.google.firebase.auth.FirebaseAuth
 
-class notes_book : AppCompatActivity(), NotesAdaptor.NoteClickListener{
+class notes_book: Fragment(), NotesAdaptor.NoteClickListener {
     private lateinit var binding: NotesBookBinding
     private lateinit var database: NotesDataBase
     lateinit var viewModel: NotesViewModel
     lateinit var adapter: NotesAdaptor
-    private var doubleBackToExitPressedOnce = false
 
-    //выход из приложения
-    override fun onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed()
-            finishAffinity() // Завершить все активности приложения
-        } else {
-            this.doubleBackToExitPressedOnce = true
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                doubleBackToExitPressedOnce = false
-            }, 2000)
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = NotesBookBinding.inflate(inflater,container,false)
+        return binding.root
     }
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = NotesBookBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         initUI()
 
-
         viewModel = ViewModelProvider(
             this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
         ).get(NotesViewModel::class.java)
 
-        viewModel.allNotes.observe(this){list ->
+        viewModel.allNotes.observe(viewLifecycleOwner){list ->
             list?.let {
                 adapter.updateList(list)
             }
         }
-        database = NotesDataBase.getDataBase(this)
+        database = NotesDataBase.getDataBase(requireContext())
     }
 
-    private fun initUI(){
 
+    private fun initUI(){
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        adapter = NotesAdaptor(this,this)
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        adapter = NotesAdaptor(requireContext(), this)
         binding.recyclerView.adapter = adapter
 
         val getContent =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-                if(result.resultCode == Activity.RESULT_OK){
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
                     val note = result.data?.getSerializableExtra("note") as? EntityDataBase
-                    if(note != null){
+                    if (note != null) {
                         viewModel.insertNote(note)
                     }
                 }
             }
 
         binding.addNote.setOnClickListener {
-            val intent = Intent(this, add_notes::class.java)
+            val intent = Intent(requireContext(), add_notes::class.java)
             getContent.launch(intent)
         }
-
     }
 
     private val UpdateOrDeleteNote =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
-            if(result.resultCode == Activity.RESULT_OK){
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
                 val note = result.data?.getSerializableExtra("note") as EntityDataBase
                 val isDelete = result.data?.getBooleanExtra("delete_note", false) as Boolean
-                if(note != null && !isDelete){
+                if (note != null && !isDelete) {
                     viewModel.updateNote(note)
-                }
-                else if(note != null && isDelete){
+                } else if (note != null && isDelete) {
                     viewModel.deleteNote(note)
                 }
             }
         }
-    override fun onNoteClicked(note: EntityDataBase){
-        val intent = Intent(this@notes_book,add_notes::class.java)
+
+    override fun onNoteClicked(note: EntityDataBase) {
+        val intent = Intent(requireContext(),add_notes::class.java)
         intent.putExtra("current_note", note)
         UpdateOrDeleteNote.launch(intent)
     }
+
 }
